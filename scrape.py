f6032322c3c58867d828005496e56f83c948ea93
eliana817 <egrasser@guardiaschool.fr>
2023-02-02 16:18:45 +0100

    scrape all articles from website

--- scrape.py
+++ scrape.py
@@ -8,31 +8,35 @@ response1 = requests.get(baseUrl)
 
 #Scrape the data (title, link and small description of the articles from the economist concerning the US midterm politics)
 
-def ecoProcess(soup):
-    div = soup.findAll('div', {"class": ["css-1a74g8a e16rqvvr0", "css-1xnjxgi e16rqvvr0"]})
+def ecoProcess(soup, content):
+    div = soup.findAll('div', {"class": ["css-1a74g8a e16rqvvr0", "css-1xnjxgi e16rqvvr0", "css-mi70rv e16rqvvr0"]})
     #print(len(div))
     for d in div:
-        h3 = d.find('h3')
-        a = h3.find('a')
-        
-        if len(a) == 2 :
-            articleTitle = a.contents[1]
-        elif len(a) == 1:
-            articleTitle = a.contents[0]
-        else:
-            articleTitle = a.contents[2]
         
         try:
-            newUrl = baseUrl + a['href']
-            date = ecoDate(newUrl)
+            h3 = d.find('h3')
+            a = h3.find('a')
+            
+            if len(a) == 2 :
+                articleTitle = a.contents[1]
+            elif len(a) == 1:
+                articleTitle = a.contents[0]
+            else:
+                articleTitle = a.contents[2]
+        
             try:
-                currentDiv = d
-                p = ecoP(currentDiv)
-                print(articleTitle, " - ", newUrl, " - ", p, " - ", date)  
-                print()
+                newUrl = baseUrl + a['href']
+                date = ecoDate(newUrl)
+                try:
+                    currentDiv = d
+                    p = ecoP(currentDiv)
+                    print(articleTitle, " - ", newUrl, " - ", p, " - ", date)  
+                    print()
+                except:
+                    print(articleTitle, " - ", newUrl, " - ", date)
+                    print()
             except:
-                print(articleTitle, " - ", newUrl, " - ", date)
-                print()
+                pass
         except:
             pass
 
@@ -48,16 +52,30 @@ def ecoP(div):
     p = div.find('p').get_text().strip()
     return p
 
-
-if response1.ok :
-    soup = BeautifulSoup(response1.text,'html.parser')
+def ecoLinks(soup):
+    links = []
+    linkContent = []
     theme = soup.findAll('ul', {'class': ['ds-navigation-list-items--section', 'is-selected', '']})
     for t in theme:
         a = t.findAll('a')
         for a in a:
             uri = a['href']
-            response2 = requests.get(baseUrl + uri)
-            if response2.ok :
-                soup2 = BeautifulSoup(response2.text,'html.parser')
-                ecoProcess(soup2)
+            links.append(uri)
+            span = a.find('span')
+            linkContent.append(span.contents[0])
+    return links, linkContent
+            
+
+if response1.ok :
+    soup = BeautifulSoup(response1.text,'html.parser')
+    links, linkContent = ecoLinks(soup)
+    for link in links:
+        contentIndex = links.index(link)
+        content = linkContent[contentIndex]
+        print("-" * 50," ", content," ", "-" * 50)
+        print()
+        response2 = requests.get(baseUrl + link)
+        if response2.ok :
+            soup2 = BeautifulSoup(response2.text,'html.parser')
+            ecoProcess(soup2, content)
 
