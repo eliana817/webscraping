a9ca26bfa78795b9460dba91e1baa73ae03b771b
unknown <egrasser@gamingcampus.local>
2023-02-20 16:01:14 +0100

    check for nan values

--- clean.py
+++ clean.py
@@ -3,7 +3,7 @@ import numpy as np
 import re
 
 data = pd.read_csv('file.csv')
-nullData = data.isnull().sum() #there are some articles which don't have a summary / paragraph
+#nullData = data.isnull().sum() #there are some articles which don't have a summary / paragraph
 duplicated = data.duplicated()
 
 for k in range(len(duplicated)):
@@ -11,5 +11,9 @@ for k in range(len(duplicated)):
     print(data['id'][k], " / ", data['theme'][k], " / ", data['title'][k]) #prints part of the content of the duplicated line
 
 #data['date'] = pd.to_datetime(data['date'], format='% %th %' or '% %st %' or '% %nd %' or '% %rd %', errors='coerce') to change the dates to a certain format
-    
+nullData = 0
+for i in range(492):
+    if data['summary'][i] == 'nan':
+        print(data['id'][i])
 
+print(nullData)
