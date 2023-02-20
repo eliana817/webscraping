5b674be32c44562626fa636be3253c36c771e051
unknown <egrasser@gamingcampus.local>
2023-02-20 16:40:18 +0100

    check and replace Nan values in the csv file

--- clean.py
+++ clean.py
@@ -11,9 +11,11 @@ for k in range(len(duplicated)):
     print(data['id'][k], " / ", data['theme'][k], " / ", data['title'][k]) #prints part of the content of the duplicated line
 
 #data['date'] = pd.to_datetime(data['date'], format='% %th %' or '% %st %' or '% %nd %' or '% %rd %', errors='coerce') to change the dates to a certain format
-nullData = 0
-for i in range(492):
-    if data['summary'][i] == 'nan':
-        print(data['id'][i])
 
-print(nullData)
+print("Number of articles with no summary: ",data['summary'].isnull().sum()) #counts number of lines with no summary
+
+mdata = data.fillna("No summary") #replace the empty data with No summary data
+
+mdata.to_csv('file.csv',index=False) #replace the original data with the new modified one: mdata
+
+#print(mdata['summary'].isnull().sum()) check that there are no more empty summaries any more 
