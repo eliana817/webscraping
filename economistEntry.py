c32c4238b300908a4d89e245b6540a50f505f8f9
eliana817 <egrasser@guardiaschool.fr>
2023-02-21 18:00:52 +0100

    create a dictionnary method

--- economistEntry.py
+++ economistEntry.py
@@ -1,7 +1,26 @@
 class economist:
-    def __innit__(self):
-        pass
-    def getEntry(self):
-        pass
+    def __init__(self, id, theme, sourceUrl, articleLink, summary, date, title):
+        self.id = id
+        self.theme = theme
+        self.sourceUrl = sourceUrl
+        self.articleLink = articleLink
+        self.summary = summary
+        self.date = date
+        self.title = title
+
+    def dictEntry(self, rows, id, c, links, h, allLinks, i, allP, allDates, allTitles):
+        row = {}
+        row[self.id] = id
+        row[self.theme] = c
+        row[self.sourceUrl] = links[h]
+
+        row[self.articleLink] = allLinks[c][i]           
+        row[self.summary] = allP[c][i]           
+        row[self.date] = allDates[c][i]       
+        row[self.title] = allTitles[c][i]
+
+        rows.append(row)
+        return rows
 
 #get + set
+
