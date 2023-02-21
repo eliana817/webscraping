d67d2e172094c31f1e182482493c12621dad21cd
eliana817 <egrasser@guardiaschool.fr>
2023-02-21 11:57:25 +0100

    delete duplicates and change the date column type

--- clean.py
+++ clean.py
@@ -3,19 +3,39 @@ import numpy as np
 import re
 
 data = pd.read_csv('file.csv')
-#nullData = data.isnull().sum() #there are some articles which don't have a summary / paragraph
+
+###################### Check and delete duplicated lines ######################## 
+
 duplicated = data.duplicated()
 
 for k in range(len(duplicated)):
    if duplicated[k] == True:
     print(data['id'][k], " / ", data['theme'][k], " / ", data['title'][k]) #prints part of the content of the duplicated line
 
+data.drop_duplicates(subset=None, inplace=True) #subset = None means that we take into account every column when checking for duplicates
+                                                #and not specific ones. inplace = True means that we remove the duplicate lines if there are any
+
+data.to_csv('file.csv', index=False) #apply the changes to the main file
+
+######################## Make sure that the dates are in the correct format #######################
+
 #data['date'] = pd.to_datetime(data['date'], format='% %th %' or '% %st %' or '% %nd %' or '% %rd %', errors='coerce') to change the dates to a certain format
+
+######################## Check for empty data and replace it with another value ######################
 
 print("Number of articles with no summary: ",data['summary'].isnull().sum()) #counts number of lines with no summary
 
-mdata = data.fillna("No summary") #replace the empty data with No summary data
+data['summary'] = data['summary'].fillna("No summary") #replace the empty data with No summary data
 
-mdata.to_csv('file.csv',index=False) #replace the original data with the new modified one: mdata
+data.to_csv('file.csv',index=False) #replace the original data with the new modified one: mdata
+
+#OR 
+
+#mdata = data.fillna('No summary') This will replace all empty data, not only in the summary column
+#mdata.to_csv('file.csv', index=False)
 
 #print(mdata['summary'].isnull().sum()) check that there are no more empty summaries any more 
+
+######################## Change the date column type ########################
+
+data['date'] = pd.to_datetime(data['date']) #change the date column type from object to date
